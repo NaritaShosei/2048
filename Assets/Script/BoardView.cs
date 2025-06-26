d8afc1348b6301976534dfdad35fe40a41850145
NaritaShosei <onigirikun7171@gmail.com>
2025-06-26 19:17:53 +0900

    [add]構造体の作成

---
+++ Assets/Script/BoardView.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoardView : MonoBehaviour
+{
+    [SerializeField] GameObject _cellContainer;
+    [SerializeField] Text _scoreText;
+
+    void Start()
+    {
+
+    }
+
+    void UpdateView()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+/// <summary>
+/// セルが存在する座標を保存する
+/// </summary>
+public struct BoardPosition : IEquatable<BoardPosition>
+{
+    int _row;
+    int _column;
+    public int Row => _row;
+    public int Column => _column;
+
+    /// <summary>
+    /// コンストラクター
+    /// </summary>
+    /// <param name="row"></param>
+    /// <param name="column"></param>
+    public BoardPosition(int row, int column)
+    {
+        _row = row;
+        _column = column;
+    }
+    /// <summary>
+    /// 等価比較
+    /// </summary>
+    /// <param name="other"></param>
+    /// <returns></returns>
+    public bool Equals(BoardPosition other)
+    {
+        return _row == other._row && _column == other._column;
+    }
+    /// <summary>
+    /// Hash値の取得
+    /// </summary>
+    /// <returns></returns>
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(_row, _column);
+    }
+    /// <summary>
+    /// 同じ値かどうか
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static bool operator ==(BoardPosition a, BoardPosition b)
+    {
+        return a.Equals(b);
+    }
+
+    /// <summary>
+    /// 違う値かどうか
+    /// </summary>
+    /// <param name="a"></param>
+    /// <param name="b"></param>
+    /// <returns></returns>
+    public static bool operator !=(BoardPosition a, BoardPosition b)
+    {
+        return !a.Equals(b);
+    }
+}
