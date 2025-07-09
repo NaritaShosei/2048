7d29a9bb1246ff8d2b66dca5bb80a53e1a33e139
NaritaShosei <onigirikun7171@gmail.com>
2025-07-10 02:54:26 +0900

    [update]タイトル管理クラス

---
+++ Assets/Script/TitleManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField] ButtonBase _startButton;
+    private void Start()
+    {
+        _startButton.OnClick += () => SceneChanger.SceneChange((int)SceneType.Ingame);
+    }
+}
