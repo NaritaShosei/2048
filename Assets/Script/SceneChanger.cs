8e27dbb989eae1ea5e79ca1502bbcaacbe1dbad6
NaritaShosei <onigirikun7171@gmail.com>
2025-07-10 02:54:06 +0900

    [add]シーン遷移

---
+++ Assets/Script/SceneChanger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public static void SceneChange(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+}
+public enum SceneType
+{
+    Title = 0,
+    Ingame = 1,
+    Result = 2,
+}
