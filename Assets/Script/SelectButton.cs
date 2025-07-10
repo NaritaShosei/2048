7a092b82572efc5a9f2bbdc51d60dc82ff1fe7c2
NaritaShosei <onigirikun7171@gmail.com>
2025-07-11 02:23:39 +0900

    [update]ボタンのアニメーション

---
+++ Assets/Script/SelectButton.cs
@@ -0,0 +1,26 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class SelectButton : ButtonBase
+{
+    [SerializeField] float _duration = 0.2f;
+    [SerializeField] float _enterScale = 1.2f;
+    [SerializeField] float _exitScale = 1f;
+
+    void Start()
+    {
+        OnMouseEnter += () => ScaleAnimation(_enterScale);
+        OnMouseExit += () => ScaleAnimation(_exitScale);
+    }
+
+    void ScaleAnimation(float scale)
+    {
+        transform.DOScale(Vector3.one * scale, _duration).SetLink(gameObject);
+    }
+
+    private void OnDisable()
+    {
+        OnMouseEnter -= () => ScaleAnimation(_enterScale);
+        OnMouseExit -= () => ScaleAnimation(_exitScale);
+    }
+}
