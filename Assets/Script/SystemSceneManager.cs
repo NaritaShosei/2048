5a0d992d171d1b6d5ad9564d117a4992b567a27d
NaritaShosei <onigirikun7171@gmail.com>
2025-07-12 13:54:06 +0900

    [add]どのシーンでも使用するものをロードできるように

---
+++ Assets/Script/SystemSceneManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SystemSceneManager : MonoBehaviour
+{
+    bool _isInitialize = false;
+
+    private void Awake()
+    {
+        if (_isInitialize)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        _isInitialize = true;
+        DontDestroyOnLoad(gameObject);
+    }
+}
